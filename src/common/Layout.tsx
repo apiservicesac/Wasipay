196cbab3631fbc40d7d7a3cde5394eaf22ae1bb2
Josuesp1620 <josuesp1620@gmail.com>
2024-04-29 05:49:25 +0200

    Change Text RedShop

--- src/common/Layout.tsx
+++ src/common/Layout.tsx
@@ -66,7 +66,7 @@ const Layout = ({ children }: any) => {
         </div>
         <CartDrawer show={show} handleDrawer={handleDrawer} />
 
-        <a href="https://wa.me/51959083710?text=Me gusta tu poto flaca, lo amo, y ti tmbn te amo" target="_blank" className="fixed w-14 h-14 bg-green-500 text-white rounded-full flex items-center justify-center text-[35px] z-10 bottom-5 right-5">
+        <a href="https://wa.me/51959083710?text=Prueba" target="_blank" className="fixed w-14 h-14 bg-green-500 text-white rounded-full flex items-center justify-center text-[35px] z-10 bottom-5 right-5">
             <i className="ri-whatsapp-line"></i>
             </a>
 </div>
