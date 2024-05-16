ac4c076797fb0a4b3c68a70107fa7e37109e70a0
Josuesp1620 <josuesp1620@gmail.com>
2024-05-16 08:20:23 -0500

    Delete Imports unused

--- src/common/Header.tsx
+++ src/common/Header.tsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { ChevronsLeft } from 'lucide-react';
 
 const Header = ({ children }: { children : React.ReactNode }) => {    
 
