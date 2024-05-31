5de8df9df3860b6a69f72329364e10dca1b9afc9
Josuesp1620 <josuesp1620@gmail.com>
2024-05-31 18:38:36 -0500

    Add Panel Tab Profile Edit

--- src/common/components/Tab/NavItemCustom.tsx
+++ src/common/components/Tab/NavItemCustom.tsx
@@ -4,11 +4,11 @@ export const overviewTabs = `
 inline-block px-4 py-2 text-base transition-all duration-300 ease-linear rounded-t-md text-slate-500 dark:text-zink-200 border-b border-transparent group-[.active]:text-custom-500 dark:group-[.active]:text-custom-500 group-[.active]:border-b-custom-500 dark:group-[.active]:border-b-custom-500 hover:text-custom-500 dark:hover:text-custom-500 active:text-custom-500 dark:active:text-custom-500 -mb-[1px]
 `
 
-const NavItemCustom = ({ label } : { label: string}) => {
+const NavItemCustom = ({ label, eventKey } : { label: string, eventKey: string }) => {
 
     return (
-        <Nav.Item eventKey="overviewTabs" className="group">
-            <a href="" data-tab-toggle data-target="overviewTabs" className={overviewTabs}>{label}</a>
+        <Nav.Item eventKey={eventKey}  className="group">
+            <a href="#!" data-tab-toggle data-target={eventKey} className={overviewTabs}>{label}</a>
         </Nav.Item>  
     );
 }
--- src/shop/infrastructure/driving-adapter/views/public_profile/index.tsx
+++ src/shop/infrastructure/driving-adapter/views/public_profile/index.tsx
@@ -11,18 +11,22 @@ const ShopComponent = () => {
     return (
         <Layout>
             <React.Fragment>
-                <Tab.Container defaultActiveKey="overviewTabs">
+                <Tab.Container defaultActiveKey="store-tab">
                     <div className="mt-0 rounded-t-none card">
                         <ShopProfileInfo  className="card-body !px-2.5"/>
                         <div className="card-body !px-2.5 !py-0">
                             <Nav className="flex flex-wrap w-full text-sm font-medium text-center nav-tabs items-center justify-center">
-                                <NavItemCustom label="Store"/>  
+                                <NavItemCustom label="Store" eventKey={'store-tab'}/>  
+                                <NavItemCustom label="Profile" eventKey={'profile-tab'}/>  
                             </Nav>
                         </div>
                     </div>
                     <Tab.Content className="tab-content">
-                        <Tab.Pane eventKey="overviewTabs" id="overviewTabs">                            
+                        <Tab.Pane eventKey="store-tab" id="store-tab">                            
                             <Catalog />
+                        </Tab.Pane>
+                        <Tab.Pane eventKey="profile-tab" id="profile-tab">                            
+                            profile
                         </Tab.Pane>
                     </Tab.Content>
                 </Tab.Container>
