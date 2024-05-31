f6346294682f54610d40b67760065416eead6bf4
Josuesp1620 <josuesp1620@gmail.com>
2024-05-30 23:10:37 -0500

    Update Widgets Dashbaord

--- src/admin/views/components/Widgets.tsx
+++ src/admin/views/components/Widgets.tsx
@@ -1,46 +1,46 @@
 import React from 'react';
-import { Package, PackageX, Truck, Wallet2 } from 'lucide-react';
+import { Package, Truck, View, Wallet2 } from 'lucide-react';
 import CountUp from 'react-countup';
 
 const Widgets = () => {
     return (
         <React.Fragment>
-            <div className="col-span-12 card md:col-span-6 lg:col-span-3 2xl:col-span-2">
+            <div className="col-span-12 card md:col-span-6 lg:col-span-3 2xl:col-span-2 cursor-pointer">
                 <div className="text-center card-body">
                     <div className="flex items-center justify-center mx-auto rounded-full size-14 bg-custom-100 text-custom-500 dark:bg-custom-500/20">
                         <Wallet2 />
                     </div>
-                    <h5 className="mt-4 mb-2">$
-                        <CountUp end={236.18} decimals={2} className="counter-value" />
-                        k</h5>
+                    <h5 className="mt-4 mb-2">S/.
+                        <CountUp end={100} decimals={2} className="counter-value" />
+                    </h5>
                     <p className="text-slate-500 dark:text-zink-200">Total Revenue</p>
                 </div>
             </div>
-            <div className="col-span-12 card md:col-span-6 lg:col-span-3 2xl:col-span-2">
+            <div className="col-span-12 card md:col-span-6 lg:col-span-3 2xl:col-span-2 cursor-pointer">
                 <div className="text-center card-body">
-                    <div className="flex items-center justify-center mx-auto text-purple-500 bg-purple-100 rounded-full size-14 dark:bg-purple-500/20">
-                        <Package />
+                    <div className="flex items-center justify-center mx-auto text-purple-500 bg-purple-100 rounded-full size-14 dark:bg-purple-500/20">                        
+                        <Truck />
                     </div>
                     <h5 className="mt-4 mb-2"><CountUp end={13461} className="counter-value" /></h5>
                     <p className="text-slate-500 dark:text-zink-200">Total Orders</p>
                 </div>
             </div>
-            <div className="col-span-12 card md:col-span-6 lg:col-span-3 2xl:col-span-2">
+            <div className="col-span-12 card md:col-span-6 lg:col-span-3 2xl:col-span-2 cursor-pointer">
                 <div className="text-center card-body">
                     <div className="flex items-center justify-center mx-auto text-green-500 bg-green-100 rounded-full size-14 dark:bg-green-500/20">
-                        <Truck />
+                        <Package />
                     </div>
-                    <h5 className="mt-4 mb-2"><CountUp end={17150} className="counter-value" /></h5>
-                    <p className="text-slate-500 dark:text-zink-200">Delivered</p>
+                    <h5 className="mt-4 mb-2"><CountUp end={10} className="counter-value" /></h5>
+                    <p className="text-slate-500 dark:text-zink-200">Total Products</p>
                 </div>
             </div>
-            <div className="col-span-12 card md:col-span-6 lg:col-span-3 2xl:col-span-2">
+            <div className="col-span-12 card md:col-span-6 lg:col-span-3 2xl:col-span-2 cursor-pointer">
                 <div className="text-center card-body">
                     <div className="flex items-center justify-center mx-auto text-red-500 bg-red-100 rounded-full size-14 dark:bg-red-500/20">
-                        <PackageX />
+                        <View />
                     </div>
                     <h5 className="mt-4 mb-2"><CountUp end={3519} className="counter-value" /></h5>
-                    <p className="text-slate-500 dark:text-zink-200">Cancelled</p>
+                    <p className="text-slate-500 dark:text-zink-200">Total Views</p>
                 </div>
             </div>
         </React.Fragment>
