30ad157752a642a601c1ae2cedde3ca9cc313aaf
Josuesp1620 <josuesp1620@gmail.com>
2024-05-31 20:11:01 -0500

    Add Dependencies - Register View Page

---
+++ src/Auth/views/register/index.tsx
@@ -0,0 +1,121 @@
+import React from "react";
+import AuthIcon from "@/Auth/components/AuthIcon";
+import { Link } from "react-router-dom";
+
+// Formik validation
+import * as Yup from "yup";
+import { useFormik as useFormic } from "formik";
+
+import { Mail } from "lucide-react";
+
+export const RegisterView = () => {
+
+    document.title = "Register | Tailwick - React Admin & Dashboard Template";
+
+    const validation: any = useFormic({
+        // enableReinitialize : use this flag when initial values needs to be changed
+        enableReinitialize: true,
+
+        initialValues: {
+            email: "admin@themesbrand.com",
+            username: "admin" || '',
+            password: "123456" || '',
+        },
+        validationSchema: Yup.object({
+            email: Yup.string().email().required("Please Enter Your Email"),
+            username: Yup.string().required("Please Enter Your Username"),
+            password: Yup.string().required("Please Enter Your Password"),
+        }),
+        onSubmit: (values: any) => {
+            console.log(values)
+        }
+    });
+
+    React.useEffect(() => {
+        const bodyElement = document.body;
+
+        bodyElement.classList.add('flex', 'items-center', 'justify-center', 'min-h-screen', 'py-16', 'lg:py-10', 'bg-slate-50', 'dark:bg-zink-800', 'dark:text-zink-100', 'font-public');
+
+        return () => {
+            bodyElement.classList.remove('flex', 'items-center', 'justify-center', 'min-h-screen', 'py-16', 'lg:py-10', 'bg-slate-50', 'dark:bg-zink-800', 'dark:text-zink-100', 'font-public');
+        }
+    }, []);
+
+    return (
+        <React.Fragment>
+            <div className="relative">
+
+                <AuthIcon />
+
+                <div className="mb-0 w-screen lg:w-[500px] card shadow-lg border-none shadow-slate-100 relative">
+                    <div className="!px-10 !py-12 card-body">
+                        <Link to="/">
+                            <img src='https://www.redshop.io/images/logo.png' alt="" className="block h-10 mx-auto dark:hidden" />
+                        </Link>
+
+                        <div className="mt-8 text-center">
+                            <h4 className="mb-1 text-custom-500 dark:text-custom-500">Create your free account</h4>
+                            <p className="text-slate-500 dark:text-zink-200">Get your free Tailwick account now</p>
+                        </div>
+
+                        <form action="/" className="mt-10" id="registerForm"
+                            onSubmit={(event: any) => {
+                                event.preventDefault();
+                                validation.handleSubmit();
+                                return false;
+                            }}>
+                            <div className="mb-3">
+                                <label htmlFor="email-field" className="inline-block mb-2 text-base font-medium">Email</label>
+                                <input
+                                    type="text"
+                                    id="email-field"
+                                    name="email"
+                                    className="form-input border-slate-200 dark:border-zink-500 focus:outline-none focus:border-custom-500 disabled:bg-slate-100 dark:disabled:bg-zink-600 disabled:border-slate-300 dark:disabled:border-zink-500 dark:disabled:text-zink-200 disabled:text-slate-500 dark:text-zink-100 dark:bg-zink-700 dark:focus:border-custom-800 placeholder:text-slate-400 dark:placeholder:text-zink-200"
+                                    placeholder="Enter email"
+                                    onChange={validation.handleChange}
+                                    onBlur={validation.handleBlur}
+                                    value={validation.values.email || ""} />
+                                {validation.touched.email && validation.errors.email ? (
+                                    <div id="email-error" className="mt-1 text-sm text-red-500">{validation.errors.email}</div>
+                                ) : null}
+                            </div>
+                            <div className="mb-3">
+                                <label htmlFor="password" className="inline-block mb-2 text-base font-medium">Password</label>
+                                <input
+                                    type="password"
+                                    id="password"
+                                    name="password"
+                                    className="form-input border-slate-200 dark:border-zink-500 focus:outline-none focus:border-custom-500 disabled:bg-slate-100 dark:disabled:bg-zink-600 disabled:border-slate-300 dark:disabled:border-zink-500 dark:disabled:text-zink-200 disabled:text-slate-500 dark:text-zink-100 dark:bg-zink-700 dark:focus:border-custom-800 placeholder:text-slate-400 dark:placeholder:text-zink-200"
+                                    placeholder="Enter password"
+                                    onChange={validation.handleChange}
+                                    onBlur={validation.handleBlur}
+                                    value={validation.values.password || ""} />
+                                {validation.touched.password && validation.errors.password ? (
+                                    <div id="password-error" className="mt-1 text-sm text-red-500">{validation.errors.password}</div>
+                                ) : null}
+                            </div>
+                            <p className="italic text-15 text-slate-500 dark:text-zink-200">By registering you agree to the Tailwick <a href="#!" className="underline">Terms of Use</a></p>
+                            <div className="mt-10">
+                                <button type="submit" className="w-full text-white transition-all duration-200 ease-linear btn bg-custom-500 border-custom-500 hover:text-white hover:bg-custom-600 hover:border-custom-600 focus:text-white focus:bg-custom-600 focus:border-custom-600 focus:ring focus:ring-custom-100 active:text-white active:bg-custom-600 active:border-custom-600 active:ring active:ring-custom-100 dark:ring-custom-400/20">Create Account</button>
+                            </div>
+
+                            <div className="relative text-center my-9 before:absolute before:top-3 before:left-0 before:right-0 before:border-t before:border-t-slate-200 dark:before:border-t-zink-500">
+                                <h5 className="inline-block px-2 py-0.5 text-sm bg-white text-slate-500 dark:bg-zink-600 dark:text-zink-200 rounded relative">Create account with</h5>
+                            </div>
+
+                            <div className="flex flex-wrap justify-center gap-2">                                
+                                <button type="button" className="flex items-center justify-center size-[37.5px] transition-all duration-200 ease-linear p-0 text-white btn bg-orange-500 border-orange-500 hover:text-white hover:bg-orange-600 hover:border-orange-600 focus:text-white focus:bg-orange-600 focus:border-orange-600 active:text-white active:bg-orange-600 active:border-orange-600">
+                                    <Mail className="size-4"></Mail>
+                                </button>
+                            </div>
+
+                            <div className="mt-10 text-center">
+                                <p className="mb-0 text-slate-500 dark:text-zink-200">Already have an account ? <Link to="/login" className="font-semibold underline transition-all duration-150 ease-linear text-slate-500 dark:text-zink-200 hover:text-custom-500 dark:hover:text-custom-500">Login</Link> </p>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </React.Fragment>
+    );
+}
--- src/core/routes/allRoutes.ts
+++ src/core/routes/allRoutes.ts
@@ -1,5 +1,6 @@
 import { LoginView } from "@/Auth/views/login";
 import { LogoutView } from "@/Auth/views/logout";
+import { RegisterView } from "@/Auth/views/register";
 import AdminView from "@/admin/views";
 import { ProductsView } from "@/admin/views/products";
 import { CreateProductView } from "@/admin/views/products/create";
@@ -14,6 +15,7 @@ interface RouteObject {
 const publicRoutes: Array<RouteObject> = [
     { path: "/", component: ShopView },
     { path: "/login", component: LoginView },
+    { path: "/register", component: RegisterView },
     { path: "/logout", component: LogoutView },
 ]
 
