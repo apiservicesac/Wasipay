20c32b43e022f4168e9ca0d99898d43c83afcd8e
Josuesp1620 <josuesp1620@gmail.com>
2024-06-30 22:40:35 -0500

    Fix OrderLine Repository

--- src/shop/domain/repositories/OrderLine.ts
+++ src/shop/domain/repositories/OrderLine.ts
@@ -3,6 +3,7 @@ import { OrderLineEntity as Entity } from '../entities'
 export interface OrderLineRepository {
     getById: (id: string) => Promise<Entity | null>
     
+    save: (entities: Entity[]) => Promise<Entity[] | null>
     update: (id: string, entity: Entity) => Promise<Entity | null>
     update_field: (id : string, field : string, value : any) => Promise<Entity | null>
 
