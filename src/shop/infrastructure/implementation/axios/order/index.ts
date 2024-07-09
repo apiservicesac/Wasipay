d2965a52bf5041cc1c6e5c51e80b134db62817d4
Josuesp1620 <josuesp1620@gmail.com>
2024-07-08 21:39:47 -0500

    Update Create Order Implementation

--- src/shop/infrastructure/implementation/axios/order/index.ts
+++ src/shop/infrastructure/implementation/axios/order/index.ts
@@ -1,3 +1,4 @@
+import { CustomReponse } from "@/shared/entities/Response";
 import { OrderEntity as Entity } from "@/shop/domain/entities";
 import { OrderRepository as Repository } from "@/shop/domain/repositories";
 import { OrderAxios as Axios } from '@/shop/infrastructure/driven-adapter/axios/order'
@@ -5,8 +6,8 @@ import { OrderAxios as Axios } from '@/shop/infrastructure/driven-adapter/axios/
 class ImplementationAxios implements Repository {
 
     async getAll(shop_id: string): Promise<Entity[]> {
-        const { data }: { data: Entity[] } = await Axios.getAll(shop_id);
-        return data;
+        const response : CustomReponse<Entity[]> = await Axios.getAll(shop_id);
+        return response.data as Entity[];
     }
     
     async save (_data: Entity): Promise<Entity | null> {
@@ -14,8 +15,8 @@ class ImplementationAxios implements Repository {
             const body : Entity = {
                 ..._data
             }
-            const { data } : { data: Entity } = await Axios.create(body._id!, body);
-            return data
+            const response : CustomReponse<Entity> = await Axios.create(body.shop_id!, body);
+            return response.data as Entity;
         }catch {
             return null
         }
@@ -23,8 +24,8 @@ class ImplementationAxios implements Repository {
 
     async update(id: string, _data: Entity): Promise<Entity | null> {
         try{
-            const { data } : { data: Entity } = await Axios.update(id, _data);
-            return data
+            const response : CustomReponse<Entity> = await Axios.update(id, _data);
+            return response.data as Entity;
         }catch {
             return null
         }
@@ -32,8 +33,8 @@ class ImplementationAxios implements Repository {
     
     async update_field(id: string, field: string, value: any): Promise<Entity | null> {
         try{
-            const { data } : { data: Entity } = await Axios.update_field(id, {field, value});
-            return data
+            const response : CustomReponse<Entity> = await Axios.update_field(id, {field, value});
+            return response.data as Entity;
         }catch {
             return null
         }
@@ -48,19 +49,19 @@ class ImplementationAxios implements Repository {
     }
 
     async getById(id: string): Promise<Entity | null> {
-        const { data } : { data: Entity } = await Axios.getById(id);
+        const response : CustomReponse<Entity> = await Axios.getById(id);
     
-        if (!data) return null;
+        if (!response.data) return null;
     
-        return data
+        return response.data as Entity;
     }
     
     async getNextCode(shop_id: string): Promise<string | null> {
-        const { data }: { data: string } = await Axios.getNextCode(shop_id);
+        const response : CustomReponse<string> = await Axios.getNextCode(shop_id);
     
-        if (!data) return null;
+        if (!response.data) return null;
     
-        return data
+        return response.data as string;
     }
     
 }
