e882938a666b7ac646b895a918b41154cf8a0e49
Josuesp1620 <josuesp1620@gmail.com>
2024-07-08 21:38:26 -0500

    Update Address Entity Domain

--- src/shop/domain/entities/Address.ts
+++ src/shop/domain/entities/Address.ts
@@ -1,21 +1,14 @@
 export interface AddressEntity {
-    _id?: string;
+    id?: string;
     first_name?: string;
     last_name?: string,
     phone_number?: string,
-    email?: string;
-    customer_id?: string;
+    email?: string;    
     street?: string;
     city?: string;
     state?: string;
     postal_code?: string;
-    coordinates?: Coordinates;
     country?: string;
     createdAt?: Date;
     updatedAt?: Date;
-}
-
-interface Coordinates {
-    latitude: number,
-    longitude: number
 }
