14fdaee26a12a5a757ae13362222d7d42bc01f5f
Josuesp1620 <josuesp1620@gmail.com>
2024-07-14 15:50:31 -0500

    [Update] Product Overview Add to Cart

--- src/shop/infrastructure/driving-adapter/public/views/home/components/product_overview/index.tsx
+++ src/shop/infrastructure/driving-adapter/public/views/home/components/product_overview/index.tsx
@@ -2,16 +2,19 @@ import React from "react";
 
 
 // Icon
-import { ShoppingCart } from 'lucide-react';
+import { Minus, Plus, ShoppingCart } from 'lucide-react';
 import { Link } from "react-router-dom";
 import BreadCrumb from "@/common/BreadCrumb";
 import { LayoutPublic } from "@/common/layout_2";
 import { ProductOverViewHelper } from "./helper";
 import { formatCurrency, formatDiscountPrice } from "@/common/utils/format";
-
+import { CartHelper } from "@/common/components/cart/helper";
 
 const ProductOverView = () => {
     const helper = ProductOverViewHelper()
+
+    const { addProductCart, decreaseProductQuantity, increaseProductQuantity } = CartHelper()
+
 
     return (
         <LayoutPublic>
@@ -45,11 +48,30 @@ const ProductOverView = () => {
                                 </div>
 
                                 <div className="flex gap-2 mt-4 shrink-0">
-                                    <Link to="/apps-ecommerce-cart" type="button" className="w-full bg-white border-dashed text-custom-500 btn border-custom-500 hover:text-custom-500 hover:bg-custom-50 hover:border-custom-600 focus:text-custom-600 focus:bg-custom-50 focus:border-custom-600 active:text-custom-600 active:bg-custom-50 active:border-custom-600 dark:bg-zink-700 dark:ring-custom-400/20 dark:hover:bg-custom-800/20 dark:focus:bg-custom-800/20 dark:active:bg-custom-800/20">
+                                    
+                                {helper.product?.in_cart ? (
+                                    <div className="flex items-center justify-between gap-3 w-full">
+                                        <div className="inline-flex text-center input-step">
+                                            <button onClick={() => decreaseProductQuantity(helper.product?.product?.id!)} type="button" className="border size-9 leading-[15px] minus bg-white dark:bg-zink-700 dark:border-zink-500 rounded-l transition-all duration-200 ease-linear border-slate-200 text-slate-500 dark:text-zink-200 hover:bg-custom-500 dark:hover:bg-custom-500 hover:text-custom-50 dark:hover:text-custom-50 hover:border-custom-500 dark:hover:border-custom-500 focus:bg-custom-500 dark:focus:bg-custom-500 focus:border-custom-500 dark:focus:border-custom-500 focus:text-custom-50 dark:focus:text-custom-50">
+                                                <Minus className="inline-block size-4"></Minus>
+                                            </button>
+                                            <input type="number" className="w-12 text-center h-9 border-y product-quantity dark:bg-zink-700 focus:shadow-none dark:border-zink-500" value={helper.product?.quantity} min="0" max="100" readOnly />
+                                            <button onClick={() => increaseProductQuantity(helper.product?.product?.id!)} type="button" className="transition-all duration-200 ease-linear bg-white border dark:bg-zink-700 dark:border-zink-500 rounded-r size-9 border-slate-200 plus text-slate-500 dark:text-zink-200 hover:bg-custom-500 dark:hover:bg-custom-500 hover:text-custom-50 dark:hover:text-custom-50 hover:border-custom-500 dark:hover:border-custom-500 focus:bg-custom-500 dark:focus:bg-custom-500 focus:border-custom-500 dark:focus:border-custom-500 focus:text-custom-50 dark:focus:text-custom-50">
+                                                <Plus className="inline-block size-4"></Plus>
+                                            </button>
+                                        </div>
+                                    </div>
+                                ) : (
+                                                                    
+                                    <button onClick={() => addProductCart(helper.product!)} type="button" className="w-full bg-white border-dashed text-custom-500 btn border-custom-500 hover:text-custom-500 hover:bg-custom-50 hover:border-custom-600 focus:text-custom-600 focus:bg-custom-50 focus:border-custom-600 active:text-custom-600 active:bg-custom-50 active:border-custom-600 dark:bg-zink-700 dark:ring-custom-400/20 dark:hover:bg-custom-800/20 dark:focus:bg-custom-800/20 dark:active:bg-custom-800/20">
                                         <ShoppingCart className="inline-block size-3 align-middle ltr:mr-1 rtl:ml-1" />
                                         <span className="align-middle">Add to Cart</span>
-                                    </Link>
-                                    <button type="button" className="w-full text-white bg-red-500 border-red-500 btn hover:text-white hover:bg-red-600 hover:border-red-600 focus:text-white focus:bg-red-600 focus:border-red-600 focus:ring focus:ring-red-100 active:text-white active:bg-red-600 active:border-red-600 active:ring active:ring-red-100 dark:ring-red-400/20">Buy Now</button>
+                                    </button>
+
+                                )}
+
+                                   
+                                    <Link to={'/checkout'} onClick={() => addProductCart(helper.product!)} type="button" className="w-full text-white bg-red-500 border-red-500 btn hover:text-white hover:bg-red-600 hover:border-red-600 focus:text-white focus:bg-red-600 focus:border-red-600 focus:ring focus:ring-red-100 active:text-white active:bg-red-600 active:border-red-600 active:ring active:ring-red-100 dark:ring-red-400/20">Buy Now</Link>
                                 </div>                                
                             </div>
                         </div>
